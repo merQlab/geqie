f86e17104d2cdf72af153af5cd4d137e79178f32
Michał Kordasz <mkordasz@polsl.pl>
2024-11-25 19:08:42 +0100

    Last fixes

--- geqie/encodings/neqr/data.py
+++ geqie/encodings/neqr/data.py
@@ -3,7 +3,7 @@ from qiskit.quantum_info import Statevector
 
 def data(u: int, v: int, R: int, image: np.ndarray) -> Statevector:
     m = u * image.shape[0] + v
-    data_vector = np.zeros(2**(2*R))
+    data_vector = np.zeros(2**(2 * R))
     data_vector[m] = 1
 
     return Statevector(data_vector)
