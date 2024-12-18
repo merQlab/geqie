52a31bf3a77e3c5f3d0c70e8923b1a9af05e93ac
Michał Kordasz <mkordasz@polsl.pl>
2024-12-18 09:48:18 +0100

    Clean up

--- geqie/encodings/ifrqi/init.py
+++ geqie/encodings/ifrqi/init.py
@@ -4,5 +4,5 @@ from qiskit.quantum_info import Statevector
 
 
 def init(n_qubits: int) -> Statevector:
-    state = np.tile([1, 0], 2**(n_qubits-4)) # np.tile([1, 0], 2**(n_qubits-4))
+    state = np.tile([1, 0], 2**(n_qubits-4))
     return Statevector(state)
