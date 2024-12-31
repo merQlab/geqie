263c2d8fa48defdde1dafa76ca11d3b473d13c0b
rafalpotempa <rafal.potem@gmail.com>
2024-12-31 13:23:23 +0100

    Fix test

--- tests/encodings/test_frqi.py
+++ tests/encodings/test_frqi.py
@@ -8,6 +8,6 @@ from geqie.encodings import frqi
 def test_frqi():
     image = Image.open("assets/test_image_4x4.png")
     image = ImageOps.grayscale(image)
-    image = np.asarray(image) / 255.0
+    image = np.asarray(image)
     circuit = geqie.encode(frqi.init_function, frqi.data_function, frqi.map_function, image)
     geqie.simulate(circuit, 1024)
