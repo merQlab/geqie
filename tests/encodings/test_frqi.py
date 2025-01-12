0554e4e0e12cbe3ec94832eca583109608125f20
rafalpotempa <rafal.potem@gmail.com>
2025-01-12 16:42:04 +0100

    Fix frqi test

--- tests/encodings/test_frqi.py
+++ tests/encodings/test_frqi.py
@@ -6,7 +6,7 @@ import geqie
 from geqie.encodings import frqi
 
 def test_frqi():
-    image = Image.open("assets/test_image_4x4.png")
+    image = Image.open("assets/test_images/grayscale/test_image_4x4.png")
     image = ImageOps.grayscale(image)
     image = np.asarray(image)
     circuit = geqie.encode(frqi.init_function, frqi.data_function, frqi.map_function, image)
