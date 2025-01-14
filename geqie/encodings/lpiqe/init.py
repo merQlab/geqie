15acaf95d07afef0999bda5a3776d9c483e68a39
Michał Kordasz <mkordasz@polsl.pl>
2025-01-14 15:22:05 +0100

    Updated LPIQE

--- geqie/encodings/lpiqe/init.py
+++ geqie/encodings/lpiqe/init.py
@@ -1,9 +1,8 @@
 import numpy as np
 from qiskit.quantum_info import Statevector
 
-had_gate = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
 
 def init(n_qubits: int) -> Statevector:
-    state = np.tile([1], 2 ** n_qubits)
+    state = np.tile([1, 0], 2 ** (n_qubits - 1))
 
     return Statevector(state)
