64f3ecc26985604c6ad6b5ca693064b0a45bee79
rafalpotempa <rafal.potem@gmail.com>
2025-12-09 20:31:54 +0100

    Add aer-gpu to Colab

--- geqie/main.py
+++ geqie/main.py
@@ -6,7 +6,7 @@ import numpy as np
 from qiskit.circuit import QuantumCircuit
 from qiskit.quantum_info import Operator, Statevector
 from qiskit.result import Result
-from qiskit_aer import Aer
+from qiskit_aer import AerSimulator
 from qiskit_aer.noise import NoiseModel, thermal_relaxation_error
 from qiskit.quantum_info import SuperOp, Kraus
 
@@ -71,7 +71,7 @@ def simulate(
     **_: Dict[Any, Any],
 ) -> Dict[str, int] | Result:	
 
-    simulator = Aer.get_backend('aer_simulator')
+    simulator = AerSimulator()
 
     result = simulator.run(circuit, shots=n_shots, memory=True, noise_model=noise_model).result()
     if return_qiskit_result:
