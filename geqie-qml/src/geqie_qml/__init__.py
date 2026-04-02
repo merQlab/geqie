866de31e2b3cf6bbaf8225450c66f8f4b781bbe8
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2026-04-02 12:11:58 +0200

    Fix GUI build (#80)

--- geqie-qml/src/geqie_qml/__init__.py
+++ geqie-qml/src/geqie_qml/__init__.py
@@ -1,8 +1,2 @@
 from .layer import VQCLayer, MatrixDataset
 from .precompute import compute_and_save_circuits
-
-__all__ = [
-    "VQCLayer",
-    "MatrixDataset",
-    "compute_and_save_circuits",
-]
