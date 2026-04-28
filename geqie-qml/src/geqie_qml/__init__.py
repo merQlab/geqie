c370d28484b6e1776541d6a681ad072bf62888ea
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2026-04-28 18:28:57 +0200

    Optimize precompute step (#85)

--- geqie-qml/src/geqie_qml/__init__.py
+++ geqie-qml/src/geqie_qml/__init__.py
@@ -1,2 +1,11 @@
+import logging
+import os
+
 from .layer import VQCLayer, MatrixDataset
 from .precompute import compute_and_save_circuits
+
+LOGGER_FORMAT = "%(levelname)s %(asctime)s --- %(message)s (%(filename)s:%(lineno)d)"
+
+def setup_logging():
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    logging.basicConfig(level=getattr(logging, log_level), format=LOGGER_FORMAT)
