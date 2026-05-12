da8ed2432819a60469c72a7ceac86a54a99e3c7e
rafalpotempa <rafal.potem@gmail.com>
2026-05-12 11:30:19 +0200

    Add frqi tolerance test

--- tests/geqie/encodings/test_frqi.py
+++ tests/geqie/encodings/test_frqi.py
@@ -4,9 +4,15 @@ from PIL import Image, ImageOps
 import geqie
 from geqie.encodings import frqi
 
+from . import RELATIVE_TOLERANCE
+
+
 def test_frqi():
     image = Image.open("assets/test_images/grayscale/test_image_4x4.png")
     image = ImageOps.grayscale(image)
     image = np.asarray(image)
     circuit = geqie.encode(frqi.init_function, frqi.data_function, frqi.map_function, image)
     geqie.simulate(circuit, 1024)
+    results = geqie.simulate(circuit, 1024)
+    retrieved_image = frqi.retrieve_function(results)
+    assert np.allclose(image, retrieved_image, rtol=RELATIVE_TOLERANCE)
